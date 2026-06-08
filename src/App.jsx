dd5801613a6bd05fe368b9d238b2d61a5b96958c
Mhd-Muzammil <zamil627810@gmail.com>
2026-06-08 10:11:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
